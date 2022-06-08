72f143b8a4fdded86ced73ff25b7149cb9947157
kenh108 <kheeneyschool@gmail.com>
2022-06-08 14:21:35 +0100

    Created command_loop function

--- shell.c
+++ shell.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 
+void command_loop(void);
+char * read_line(void);
+char ** split_line(char *);
+int execute_command(char **);
+
 int main(int argc, char **argv) {
 
     // Loop for commands
 
     return EXIT_SUCCESS;
+}
+
+void command_loop(void) {
+    
+    char *line;
+    char **args;
+    int status;
+    
+    do {
+        printf("$ ");
+        line = read_line();
+        args = split_line();
+        status = execute_command();
+
+        free(line);
+        free(args);
+    } while (status);
 }
